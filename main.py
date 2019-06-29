232c01430e4921c54c442dcc69b08cfbe1747fbf
Or Bin <orbin50@gmail.com>
2019-06-30 00:00:06 +0300

    Removed heat index calculation and field

--- main.py
+++ main.py
@@ -25,27 +25,6 @@ def connect(timeout_sec=0):
     return True
 
 
-def compute_heat_index(temperature_c, humidity):
-    # Heat-Index calculator with celsius-grade
-    # Formulas are at https://en.wikipedia.org/wiki/Heat_index#Formula
-    # GregNau	2015
-    # https://github.com/gregnau/heat-index-calc
-
-    temperature_f = ((temperature_c * 9/5) + 32)
-
-    # Creating multiples of 'fahrenheit' & 'hum' values for the coefficients
-    T2 = pow(temperature_f, 2)
-    H2 = pow(humidity, 2)
-
-    # Coefficients for the calculations
-    C1 = [ -42.379, 2.04901523, 10.14333127, -0.22475541, -6.83783e-03, -5.481717e-02, 1.22874e-03, 8.5282e-04, -1.99e-06]
-
-    # Calculating heat-indexes with 3 different formula
-    heatindex_f = C1[0] + (C1[1] * temperature_f) + (C1[2] * humidity) + (C1[3] * temperature_f * humidity) + (C1[4] * T2) + (C1[5] * H2) + (C1[6] * T2 * humidity) + (C1[7] * temperature_f * H2) + (C1[8] * T2 * H2)
-
-    return round(((heatindex_f - 32) * 5/9), 2)
-
-
 # Deactivating access point. This didn't work when put inside a function :(
 ap = network.WLAN(network.AP_IF)
 ap.active(False)
@@ -60,13 +39,11 @@ while True:
         dht_sensor.measure()
         temperature = dht_sensor.temperature()
         humidity = dht_sensor.humidity()
-        heat_index = compute_heat_index(temperature, humidity)
 
         json_object = {
             "logger": config.LOGGER_ID,
             "humidity": humidity,
             "temperature_celsius": temperature,
-            "heat_index_celsius": heat_index
         }
 
         json_text = json.dumps(json_object)
